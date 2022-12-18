bfe5816465325218298a45034a102a6adef5aad7
tajinder-dhoot <singhtazinder047@gmail.com>
2022-12-18 15:21:38 -0500

    quicksort using recursion

--- quickSort.py
+++ quickSort.py
@@ -1,3 +1,6 @@
+import sys
+from load import load_numbers
+
 def quickSort(list):
 
     if(len(list) <= 1):
@@ -22,7 +25,34 @@ def split(list, target):
 
     return lower_values, higher_values
 
-numbers = [1,-6,82,2,3,8,6,4,15,12]
+# numbers = [1,-6,82,2,3,8,6,4,15,12]
 # numbers = [5,3,8,10,1]
+
+"""
+list of 10 numbers
+"""
+# numbers = load_numbers('./numbers/10_numbers.txt')
+
+"""
+list of 100 numbers
+sys.argv[1] will take arg at index 1 passed in cmd line
+so second argument passed in cmd line should be file name
+"""
+numbers = load_numbers(sys.argv[1])
+
+"""
+list of 1000 numbers
+"""
+# numbers = load_numbers('./numbers/1000_numbers.txt')
+
+"""
+list of 10000 numbers
+"""
+# numbers = load_numbers('./numbers/10000_numbers.txt')
+
+"""
+list of 1000000 numbers
+"""
+# numbers = load_numbers('./numbers/1_million_numbers.txt')
 
 print(quickSort(numbers))
